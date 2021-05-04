4004bb55abbde2850fc627f41b0331824ad3339a
Ashish <iamdeveloper.me@gmail.com>
2021-05-04 17:27:28 +0530

    Modified

--- mysite/registration/urls.py
+++ mysite/registration/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from . import views
 from django.views.generic.base import TemplateView
-from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
 	path('signup/', views.SignupView.as_view(), name='signup'),
 	path('success/', TemplateView.as_view(template_name='registration/success.html'), name='success'),
 	path('', include('django.contrib.auth.urls')),
-	path('profile/', login_required(TemplateView.as_view(template_name='registration/profile.html')), name='profile'),
+	path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- mysite/registration/views.py
+++ mysite/registration/views.py
@@ -1,5 +1,8 @@
 from .forms import RegistrationForm
 from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -8,3 +11,7 @@ class SignupView(CreateView):
     form_class = RegistrationForm
     template_name = 'registration/signup.html/'
     success_url = '/accounts/success/'
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(TemplateView):
+	template_name = 'registration/profile.html'
