645f079da4e061062de96a144d8baac48603863c
Ashish <iamdeveloper.me@gmail.com>
2021-05-19 10:47:52 +0530

    Modified

--- mysite/mysite/apps/registration/urls.py
+++ mysite/mysite/apps/registration/urls.py
@@ -3,19 +3,22 @@ from . import views
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
-	path('signup/', views.SignupView.as_view(), name='signup'),
-	path('accounts/login/', views.MyLoginView.as_view(), name='login'),
-	path('accounts/logout/', views.MyLogoutView.as_view(), name='logout'),
-	# path('', include('django.contrib.auth.urls')),
-	path('profile', views.ProfileView.as_view(), name='profile'),
+    # Without auth user
+    # path('', include('django.contrib.auth.urls')),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('accounts/login/', views.MyLoginView.as_view(), name='login'),
 
-	# Post urls
-	path('createpost/', views.PostCreateView.as_view(), name='createpost'),
-	path('', views.PostListView.as_view(), name='post'),
-	path('detail/<int:pk>', views.PostDetailView.as_view(), name='detail'),
-	path('delete/<int:pk>', views.PostDeleteView.as_view(), name='post-delete'),
-	path('deleted/', TemplateView.as_view(template_name='registration/post/post_delete.html'), name='deleted'),
-	path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    # With auth user
+    # Feed Post
+    path('', views.HomeView.as_view(), name='home'),
+    path('create/post/', views.PostCreateView.as_view(), name='create-post'),
+    path('detail/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('deleted/', TemplateView.as_view(template_name='registration/post/post_delete.html'), name='deleted'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('accounts/logout/', views.MyLogoutView.as_view(), name='logout'),
+    # Profile
+    path('profile/', views.MyProfileView.as_view(), name='profile'),
+    path('update/', views.UpdateProfileView.as_view(), name='update-profile'),
 ]
--- mysite/mysite/apps/registration/views.py
+++ mysite/mysite/apps/registration/views.py
@@ -1,16 +1,17 @@
 from .forms import RegistrationForm, NewPostForm
-from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
-from django.views.generic.base import TemplateView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView, TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
+# SignUp
 class SignupView(SuccessMessageMixin, CreateView):
     form_class = RegistrationForm
     template_name = 'registration/signup.html/'
@@ -18,20 +19,29 @@ class SignupView(SuccessMessageMixin, CreateView):
     success_message = "%(username)s was created successfully! Now Login with the same username and password!"
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileView(TemplateView):
-    template_name = 'registration/profile.html'
-
-
+# Login
 class MyLoginView(SuccessMessageMixin, LoginView):
     redirect_authenticated_user = True
     success_message = 'Welcome Here!'
 
 
+# Logout
 class MyLogoutView(LogoutView):
     template_name = 'registration/logout.html'
 
 
+# Home Page
+@method_decorator(login_required, name='dispatch')
+class HomeView(ListView):
+    model = Post
+    template_name = 'registration/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    # paginate_by = 10
+
+
+# Post Related Views
+# Post Create
 @method_decorator(login_required, name='dispatch')
 class PostCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     form_class = NewPostForm
@@ -48,21 +58,13 @@ class PostCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-@method_decorator(login_required, name='dispatch')
-class PostListView(ListView):
-    model = Post
-    template_name = 'registration/profile.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
-    # paginate_by = 10
-
-
+# Post Detail
 class PostDetailView(DetailView):
     model = Post
     template_name = 'registration/post/detail.html'
 
 
-# @method_decorator(login_required, name='dispatch')
+# Post Delete
 class PostDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'registration/post/post_confirm_delete.html'
@@ -70,10 +72,30 @@ class PostDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
     success_message = 'Your Post Deleted Successfully'
 
 
+# Post Update
 class PostUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = Post
     form_class = NewPostForm
-    template_name = 'registration/create_post.html'
+    template_name = 'registration/post/create_post.html'
     success_url = '/'
     extra_context = {'value': 'Update Post'}
-    success_message = 'Your Post Updated Successfully'
+    success_message = 'Your Post Updated Successfully!'
+
+
+# Profile Page
+# Profile
+class MyProfileView(TemplateView):
+    template_name = 'registration/profile/profile.html'
+
+
+# Profile Update
+class UpdateProfileView(UpdateView):
+    model = User
+    template_name = 'registration/profile/update_profile.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_url = '/'
+    success_message = 'Your Profile Updated Successfully!'
+
+    def get_object(self):
+        return self.request.user
+
