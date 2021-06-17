103cbbfaa25d8dc02286e7fa6355e845aa9a1baa
Ashish <iamdeveloper.me@gmail.com>
2021-06-17 20:02:58 +0530

    Udpated API written in most consice way with Djangorestframework

--- mysite/mysite/apps/registration/views.py
+++ mysite/mysite/apps/registration/views.py
@@ -176,51 +176,14 @@ class UpdateProfileView(UpdateView):
 """ APIs """
 
 from .serializers import PostSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
+from rest_framework import generics
 
 
-class PostListApiView(APIView):
-    def get(self, request, formate=None):
-        post = Post.objects.all()
-        serializer = PostSerializer(post, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostListApiView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
-class PostDetailApiView(APIView):
-    '''
-    Retrieve, update or delete a Post instance
-    '''
-
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
+class PostDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
