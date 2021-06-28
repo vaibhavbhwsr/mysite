b461c5574180fbe26e33c1d069fc01e5d9942f76
Vaibhav <Vaibhav.bhawsar.77@gmail.com>
2021-06-28 18:38:16 +0530

    Updated 28th June 2021

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,11 +24,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Mysite API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="contact@mysite.local"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
