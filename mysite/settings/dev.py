c6348644c225128804db8762cb90f07537fc133e
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-09-13 22:12:17 +0530

    Added bug for migrations folder to registration app

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -3,10 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysitedb',
+        'NAME': 'mysitedb1',
         'USER': 'postgres',
         'PASSWORD': 'psql',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5432',
     }
 }
