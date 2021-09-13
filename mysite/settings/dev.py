7933bf955a505caf9266afa1ac5e41ac2c60896f
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-09-13 22:14:11 +0530

    M

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -3,10 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysitedb1',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DATABASE_NAME', default='mysitedb'),
+        'USER': config('DATABASE_USER', default='postgres'),
+        'PASSWORD': config('DATABASE_PASSWORD', default='psql'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default=5432),
     }
 }
