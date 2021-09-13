f05f9bc3b2a4c5c2763159ccb4c27fffabbddbea
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-09-13 22:24:38 +0530

    Modified with production as prod.py file in settings

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,12 +1,1 @@
 from .base import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysitedb',
-        'USER': 'postgres',
-        'PASSWORD': 'psql',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
