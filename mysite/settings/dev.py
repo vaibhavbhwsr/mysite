43a6b666feb8909dbd7613213cc76f9720ee3a15
Vaibhav Bhawsar <Vaibhavbhawsar777@gmail.com>
2021-10-24 19:19:54 +0530

    Heroku_deployment: made changes to dev

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,4 +1,4 @@
-# from .base import *
+from .base import *
 
 # DATABASES = {
 #     'default': {
@@ -11,13 +11,3 @@
 #     }
 # }
 
-from .base import *
-import django_on_heroku
-import dj_database_url
-
-# Activate Django_on_heroku
-django_on_heroku.settings(locals())
-
-ALLOWED_HOSTS = ['mynameheresite.herokuapp.com']
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
