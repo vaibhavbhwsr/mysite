192985b8d593a85d0bcecded4b0afbee7ffa51e4
Vaibhav Bhawsar <Vaibhavbhawsar777@gmail.com>
2021-10-24 19:18:13 +0530

    Heroku_deployment: made changes to dev

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,12 +1,23 @@
-from .base import *
+# from .base import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME', default='mysitedb'),
-        'USER': config('DATABASE_USER', default='postgres'),
-        'PASSWORD': config('DATABASE_PASSWORD', default='psql'),
-        'HOST': config('DATABASE_HOST', default='localhost'),
-        'PORT': config('DATABASE_PORT', default=5432),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DATABASE_NAME', default='mysitedb'),
+#         'USER': config('DATABASE_USER', default='postgres'),
+#         'PASSWORD': config('DATABASE_PASSWORD', default='psql'),
+#         'HOST': config('DATABASE_HOST', default='localhost'),
+#         'PORT': config('DATABASE_PORT', default=5432),
+#     }
+# }
+
+from .base import *
+import django_on_heroku
+import dj_database_url
+
+# Activate Django_on_heroku
+django_on_heroku.settings(locals())
+
+ALLOWED_HOSTS = ['mynameheresite.herokuapp.com']
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
