01b4bd681526c9c5b5a3407782dd3a10e7620b93
Vaibhav Bhawsar <Vaibhavbhawsar777@gmail.com>
2021-10-24 20:07:12 +0530

    Heroku_deployment: made changes to dev

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,14 +1,18 @@
 from .base import *
 import dj_database_url
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DATABASE_NAME', default='mysitedb'),
-#         'USER': config('DATABASE_USER', default='postgres'),
-#         'PASSWORD': config('DATABASE_PASSWORD', default='psql'),
-#         'HOST': config('DATABASE_HOST', default='localhost'),
-#         'PORT': config('DATABASE_PORT', default=5432),
-#     }
-# }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME', default='mysitedb'),
+        'USER': config('DATABASE_USER', default='postgres'),
+        'PASSWORD': config('DATABASE_PASSWORD', default='psql'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default=5432),
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
