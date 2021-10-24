5ba5ea9805900de32d419ba86ad4feab2ccc9327
Vaibhav Bhawsar <Vaibhavbhawsar777@gmail.com>
2021-10-24 17:16:05 +0530

    Heroku_deployment: updated prod.py DATABASES settings.

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['mynameheresite.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME', default='postgresql-cylindrical-45367'),
+        'NAME': config('DATABASE_NAME', default='dfa0ssdi19ssr6'),
         'USER': config('DATABASE_USER', default='wppagmcsjwqzzn'),
         'PASSWORD': config('DATABASE_PASSWORD', default='f394cae348b2a1e4dea982e08dbc7fd08c75dd916d74d62c34902fb4c388b4b1'),
         'HOST': config('DATABASE_HOST', default='ec2-34-194-120-237.compute-1.amazonaws.com'),
