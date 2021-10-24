50be0e48e9528f24a5d19ffc12009fa40c3065d3
Vaibhav Bhawsar <Vaibhavbhawsar777@gmail.com>
2021-10-24 17:53:57 +0530

    Heroku_deployment: updated prod.py with DATABASES settings.

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,8 +1,14 @@
 from .base import *
 import django_on_heroku
-
+import dj_database_url
 
 # Activate Django_on_heroku
 django_on_heroku.settings(locals())
 
 ALLOWED_HOSTS = ['mynameheresite.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
