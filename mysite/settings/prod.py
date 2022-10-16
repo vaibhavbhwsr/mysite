9cf4c5da39a2aa8e36aa11fcf3ff3cfe085e2982
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2022-10-16 16:33:15 +0530

    attached aws rds specially for prod

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -41,3 +41,15 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+# Database for prod
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
+}
