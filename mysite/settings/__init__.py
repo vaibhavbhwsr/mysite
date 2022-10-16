bd05f739f325def8621a77fa646a8264e2b6f6a8
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2022-10-16 17:34:01 +0530

    adding some print command to see setting being used

--- mysite/settings/__init__.py
+++ mysite/settings/__init__.py
@@ -14,3 +14,14 @@ if MODE == 'dev':
     from .dev import *
 elif MODE == 'prod':
     from .prod import *
+
+
+# Some lines to check setting being used
+print()
+print('############################### Settings Check ################################')
+print(f'Mode: {MODE}')
+print(f'Debug: {DEBUG}')
+print(f'Static Root: {STATIC_ROOT}')
+print(f'Media Root: {MEDIA_ROOT}')
+print(f'Database: {DATABASES["default"]["HOST"]}')
+print('###############################################################################')
