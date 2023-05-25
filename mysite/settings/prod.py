cfcb826f92a9b1653355c69e30c48442c8db299b
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-05-25 08:32:08 +0530

    commented database setting in prod

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -16,17 +16,17 @@ REST_FRAMEWORK = {
 }
 
 
-# Database for prod
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
-    }
-}
+# # Database for prod
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#     }
+# }
 
 
 # Sentry settings
