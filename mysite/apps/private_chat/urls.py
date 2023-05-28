66620f6061f0ff3784fb358dbab79ff29e0d8be6
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-05-28 14:58:38 +0530

    added private page basic page

--- mysite/apps/private_chat/urls.py
+++ mysite/apps/private_chat/urls.py
@@ -5,4 +5,5 @@ from private_chat import views
 
 urlpatterns = [
     path('list/', views.UserListView.as_view(), name='user_list'),
+    path('chat/<str:username>/', views.PrivateChatView.as_view(), name='private_chat'),
 ]
--- mysite/apps/private_chat/views.py
+++ mysite/apps/private_chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from django.contrib.auth import get_user_model
 
@@ -15,3 +15,13 @@ User = get_user_model()
 class UserListView(ListView):
     model = User
     template_name = 'chat/private_chat/list.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class PrivateChatView(TemplateView):
+    template_name = 'chat/private_chat/index.html'
+
+    def get(self, request, *args, **kwargs):
+        user = User.objects.get(username=kwargs['username'])
+        kwargs['user'] = user
+        return super(PrivateChatView, self).get(request, *args, **kwargs)
