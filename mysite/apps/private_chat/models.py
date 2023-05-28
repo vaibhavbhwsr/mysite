5d4ff71e2855d49996c56a1af5c51e283803d517
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-05-28 17:02:27 +0530

    added get chat logic in private chat

--- mysite/apps/private_chat/models.py
+++ mysite/apps/private_chat/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from core.models import BaseModel
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class PrivateChat(BaseModel):
+    content = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey(
+        'OneOneGroup', on_delete=models.CASCADE, related_name='private_chat'
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+
+class OneOneGroup(BaseModel):
+    name = models.CharField(max_length=255, unique=True, null=True)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1_oog')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2_oog')
+
+    class Meta:
+        ordering = ['-id']
+        unique_together = [("user1", "user2"),]
+
+    def save(self, *args, **kwargs):
+        self.name = f"one-one-{'-'.join(sorted([str(self.user1.id), str(self.user2.id)]))}"
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f"{self.name}"
--- mysite/apps/private_chat/views.py
+++ mysite/apps/private_chat/views.py
@@ -5,6 +5,8 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView, TemplateView
 
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from private_chat.models import OneOneGroup
 
 User = get_user_model()
 
@@ -22,6 +24,15 @@ class PrivateChatView(TemplateView):
     template_name = 'chat/private_chat/index.html'
 
     def get(self, request, *args, **kwargs):
-        user = User.objects.get(username=kwargs['username'])
-        kwargs['user'] = user
+        user1 = request.user
+        user2 = User.objects.get(username=kwargs.get('username'))
+
+        try:
+            group = OneOneGroup.objects.get(
+                (Q(user1=user1) & Q(user2=user2)) | (Q(user1=user2) & Q(user2=user1))
+            )
+        except OneOneGroup.DoesNotExist:
+            group = OneOneGroup.objects.create(user1=user1, user2=user2)
+
+        kwargs['group'] = group
         return super(PrivateChatView, self).get(request, *args, **kwargs)
